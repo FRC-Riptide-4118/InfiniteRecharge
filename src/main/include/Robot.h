be0aa791613e33dce4cd5ac6ef02ff3b5a48fdc6
GavynBevington <35120045+GavynBevington@users.noreply.github.com>
2020-01-17 15:57:23 -0600

    Update Robot.h
    no

--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -9,17 +9,53 @@
 
 #include <string>
 
+#include <GenericHID.h>
+
+#include <VicorSPX.h>
+
+#include <XboxController.h>
+
+#include <DifferentialDrive.h>
+
+#include "ctre/Phoenix.h"
+
 #include <frc/TimedRobot.h>
+
 #include <frc/smartdashboard/SendableChooser.h>
 
+
 class Robot : public frc::TimedRobot {
+  frc::DifferentialDrive m_robotDrive {
+
+    frc::SpeedControllerGroup Left {
+
+      ctre::CanTalonSRX m_right_Motor{0};
+      ctre::CanTalonSRX m_backright_Motor{1};
+      ctre::VictorSPX m_topright_Motor{2}
+
+    };
+
+     frc::SpeedControllerGroup Right {
+
+      ctre::CanTalonSRX m_backLeft_Motor{3};
+      ctre::CanTalonSRX m_Left_Motor{4};
+      ctre::VictorSPX m_topleft_Motor{5};
+
+    };
+  };
+
+  frc::XboxController m_driverController{0};
+
  public:
   void RobotInit() override;
   void RobotPeriodic() override;
+
   void AutonomousInit() override;
   void AutonomousPeriodic() override;
+
   void TeleopInit() override;
   void TeleopPeriodic() override;
+  void TestInit() override;
   void TestPeriodic() override;
 
  private:
