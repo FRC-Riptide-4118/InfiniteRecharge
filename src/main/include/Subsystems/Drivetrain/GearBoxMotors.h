27d8c5fe4acefea50a13f3eb974249211a3c520d
slacksonvongunten <60015790+Captain-Flareon@users.noreply.github.com>
2020-01-26 13:34:27 -0500

    Reformated drivetrain motor names

--- src/main/include/Subsystems/Drivetrain/GearBoxMotors.h
+++ src/main/include/Subsystems/Drivetrain/GearBoxMotors.h
@@ -2,22 +2,22 @@
 #include <frc/drive/DifferentialDrive.h>
 #include <frc/SpeedControllerGroup.h>
 
-WPI_TalonSRX srx_Left_Front = {0};
-WPI_TalonSRX srx_Left_Middle = {1};
-WPI_TalonSRX srx_Left_Back = {2};
+WPI_TalonSRX srx_left_front     = {0};
+WPI_TalonSRX srx_left_middle    = {1};
+WPI_TalonSRX srx_left_back      = {2};
 
-WPI_TalonSRX srx_Right_Front = {3};
-WPI_TalonSRX srx_Right_Middle = {4};
-WPI_TalonSRX srx_Right_Back = {5};
+WPI_TalonSRX srx_right_front    = {3};
+WPI_TalonSRX srx_right_middle   = {4};
+WPI_TalonSRX srx_right_back     = {5};
 
-WPI_VictorSPX spx_Left_Front = {0};
-WPI_VictorSPX spx_Left_Middle = {1};
-WPI_VictorSPX spx_Left_Back = {2};
+WPI_VictorSPX spx_left_front    = {0};
+WPI_VictorSPX spx_left_middle   = {1};
+WPI_VictorSPX spx_left_back     = {2};
 
 
-WPI_VictorSPX spx_Right_Front = {3};
-WPI_VictorSPX spx_Right_Middle = {4};
-WPI_VictorSPX spx_Right_Back = {5};
+WPI_VictorSPX spx_right_front   = {3};
+WPI_VictorSPX spx_right_middle  = {4};
+WPI_VictorSPX spx_right_back    = {5};
 
 frc::SpeedControllerGroup left(srx_Left_Front, srx_Left_Middle, srx_Left_Back);
 frc::SpeedControllerGroup right(srx_Right_Front, srx_Right_Middle, srx_Right_Back);
