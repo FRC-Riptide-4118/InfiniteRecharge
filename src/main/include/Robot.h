f56a3cc9bcd1a2e3e52599bfc705736bcf136ef6
GavynBevington <35120045+GavynBevington@users.noreply.github.com>
2020-01-25 20:04:10 -0600

    Private class for Velocity tracking

--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -9,6 +9,13 @@
 #include "networktables/NetworkTableInstance.h"
 
 class Robot : public frc::TimedRobot {
+ private:
+  TalonFX * FX1 = new TalonFX(0);
+  frc::XboxController * Controller1 = new frc::XboxController(0);
+
+  std::string _sb;
+  int _loops = 0;
+
  public:
   void RobotInit() override;
 
