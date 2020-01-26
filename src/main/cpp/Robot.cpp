debf2c0b3719171fff3445770a1f70ed1d237c55
slacksonvongunten <60015790+Captain-Flareon@users.noreply.github.com>
2020-01-26 13:20:41 -0500

    Moved button mapping comment from Robot.cpp into Interactions.cpp

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -20,39 +20,7 @@
 #include "networktables/NetworkTable.h"
 #include "networktables/NetworkTableInstance.h"
 
-/*
-This is the mapping of all buttons on the controller:
 
-    -Left Bumper
-        - Unassigned
-    -Right Bumper
-        - Unassigned
-    -Left Joystick
-        - The Y axis of the computer for driving
-    -Right Joystick
-        - The X axis of the computer for driving/turning
-    -Right Trigger
-        - Changing the value of the Falcon 500
-    -Left Trigger
-        - Unassigned
-    -X Button
-        - Enters closed loop target positioning
-    -A Button
-        - toggleCameraMode
-    -B Button 
-        - High/low gear shifting
-    -Y Button
-        - 
-    -Left Joystick Button
-        - Unassigned
-    -Right Joystick Button
-        - Unassigned
-    -Start Button
-        - Unassigned
-    -Back Button
-        -unassigned
-
-*/
  std::shared_ptr<NetworkTable> table = nt::NetworkTableInstance::GetDefault().GetTable("limelight");
 
 
