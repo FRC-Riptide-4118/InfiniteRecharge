ce1ad36538a3ec35c144e8f6f1df66de9b95d79a
GavynBevington <35120045+GavynBevington@users.noreply.github.com>
2020-02-02 17:46:58 -0600

    Commented out Colorwheels code, Made a velocity pid loop unfinished

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -26,6 +26,8 @@
  std::shared_ptr<NetworkTable> table = nt::NetworkTableInstance::GetDefault().GetTable("limelight");
  frc::Servo falcon_Test(0);
 
+ frc::DigitalInput limitSwitch_Test {1};
+
 
 void Robot::RobotInit() {
     shifter = new GearShifter();
@@ -35,27 +37,25 @@ void Robot::RobotInit() {
     Controller2 = new frc::XboxController(1);
     interaction = new Interactions( Controller1, Controller2 );
 
+
     FX1->ConfigFactoryDefault();
 
-    //Sets the peak  and nominal outputs, 12v   
+    FX1->ConfigSelectedFeedbackSensor(TalonFXFeedbackDevice::IntegratedSensor);
+    FX1->SetSensorPhase(true);
 
-    FX1->ConfigNominalOutputForward(1, kTimeoutMs);
-    FX1->ConfigNominalOutputReverse(-1, kTimeoutMs);
+    FX1->ConfigNominalOutputForward(0, kTimeoutMs);
+    FX1->ConfigNominalOutputReverse(0, kTimeoutMs);
     FX1->ConfigPeakOutputForward(1, kTimeoutMs);
     FX1->ConfigPeakOutputReverse(-1, kTimeoutMs);
 
-    //Set closed loop gains in slot 0
-
-    FX1->Config_kF(kPIDLoopIdx, 0.0, kTimeoutMs);
-    FX1->Config_kP(kPIDLoopIdx, 0.0, kTimeoutMs);
+    FX1->Config_kF(kPIDLoopIdx, 0.1097, kTimeoutMs);
+    FX1->Config_kP(kPIDLoopIdx, 0.22, kTimeoutMs);
     FX1->Config_kI(kPIDLoopIdx, 0.0, kTimeoutMs);
     FX1->Config_kD(kPIDLoopIdx, 0.0, kTimeoutMs);
-
 
 //Initial speed of the motors
         drive.ArcadeDrive(0, 0, 0);
 //sensor setup
-     FX1->ConfigSelectedFeedbackSensor(TalonFXFeedbackDevice::IntegratedSensor);
      srx_right_back.ConfigSelectedFeedbackSensor(FeedbackDevice::CTRE_MagEncoder_Relative, 0);
      srx_left_back.ConfigSelectedFeedbackSensor(FeedbackDevice::CTRE_MagEncoder_Relative, 0);
 
@@ -92,38 +92,36 @@ void Robot::toggleCameraMode() {
 void Robot::TeleopPeriodic() {
 
     // get gamepad axis
-    const double AMPS_CHANGE_DIRECTION = 30;    
-    double motorOutput = FX1->GetOutputCurrent();
+    double TriggerAxis = Controller1->GetTriggerAxis(frc::GenericHID::Joystickhand::kLeftHand);   
+    double motorOutput = FX1->GetMotorOutputPercent();
     std::string _sb;
 
-    /* prepare line to print */
-		_sb.append("\tout:");
-		_sb.append(std::to_string(motorOutput));
-		_sb.append("\tcur:");
-		_sb.append(std::to_string(FX1->GetOutputCurrent()));
-        std::cout << FX1->GetSelectedSensorVelocity() << std::endl;
-		/* on x press enter closed-loop mode on target position */
-		if (interaction->enterPIDFXClosedLoop()) {
-			/* Position mode - button just pressed */
-            std::cout << "enteredPIDFXClosedLoop" << std::endl;
-			FX1->Set(ControlMode::Current, interaction->shooterRawSpeed() * AMPS_CHANGE_DIRECTION); /* 40 Amps in either direction */
-		} else {
-			FX1->Set(ControlMode::PercentOutput, interaction->shooterRawSpeed());
-		}
-		/* if Talon is in position closed-loop, print some more info */
-		if (FX1->GetControlMode() == ControlMode::Current) {
-			/* append more signals to print when in speed mode. */
-			_sb.append("\terrNative:");
-			_sb.append(std::to_string(FX1->GetClosedLoopError(kPIDLoopIdx)));
-			_sb.append("\ttrg:");
-			_sb.append(std::to_string(interaction->shooterRawSpeed() * AMPS_CHANGE_DIRECTION));
-		}
-		/* print every ten loops, printing too much too fast is generally bad for performance */
-		if (++_loops >= 10) {
-			_loops = 0;
-			printf("%s\n", _sb.c_str());
-		}
-		_sb.clear();
+    _sb.append("\tout");
+    _sb.append(std::to_string(motorOutput));
+    _sb.append("\tspd: ");
+    _sb.append(std::to_string(FX1->GetSelectedSensorVelocity(kPIDLoopIdx)));
+
+    if(Interactions::enterPIDFXClosedLoop) {
+
+        double targetVelocity_UnitsPer100Ms = TriggerAxis * 500.0 * 4096 / 600;
+
+        FX1->Set(ControlMode::Velocity, targetVelocity_UnitsPer100Ms);
+
+
+        _sb.append("\terrNative:");
+        _sb.append(std::to_string(FX1->GetClosedLoopError(kPIDLoopIdx)));
+        _sb.append("\ttrg:");
+        _sb.append(std::to_string(targetVelocity_UnitsPer100Ms));
+    } else {
+
+        FX1->Set(ControlMode::PercentOutput, TriggerAxis);
+    }
+
+    if (++_loops >= 10) {
+        _loops = 0;
+        printf("%s\n", _sb.c_str());
+    }
+   
 
 //   std::cout << "Left Sensor Velocity: " << srx_Left_Back.GetSelectedSensorVelocity() << std::endl;
 
@@ -145,22 +143,17 @@ void Robot::TeleopPeriodic() {
 
     // drive.ArcadeDrive(Drive, Turn, true);
 
-    if (FX1->GetSelectedSensorVelocity() >= 20000) {
-        falcon_Test.Set(1);
-    } else {
-        falcon_Test.Set(0);
-    }
 
+// ctre::pheonix::sensors::PigeonIMU::PigeonIMU(srx_left_back);
 
+//     // SmartDashboard telemetry
+//     frc::SmartDashboard::PutNumber("Red", detectedColor.red);
+//     frc::SmartDashboard::PutNumber("Green", detectedColor.green);
+//     frc::SmartDashboard::PutNumber("Blue", detectedColor.blue);
+//     frc::SmartDashboard::PutString("Color Decision", colorString);
+//     frc::SmartDashboard::PutNumber("IR", IR);
 
-    // SmartDashboard telemetry
-    frc::SmartDashboard::PutNumber("Red", detectedColor.red);
-    frc::SmartDashboard::PutNumber("Green", detectedColor.green);
-    frc::SmartDashboard::PutNumber("Blue", detectedColor.blue);
-    frc::SmartDashboard::PutString("Color Decision", colorString);
-    frc::SmartDashboard::PutNumber("IR", IR);
-
-}
+// }
 
 void Robot::TestInit() {}
 void Robot::TestPeriodic() {}
