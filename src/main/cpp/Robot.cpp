953e3889d63d0fa36ffe3aa303ad49ef8bcf5df8
GavynBevington <35120045+GavynBevington@users.noreply.github.com>
2020-02-08 00:21:41 -0500

    commented out vision tracking

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -155,29 +155,29 @@ void Robot::TeleopPeriodic() {
     bool putColor = true;
     matcher->putDashboardTelemetry(putColor);
 
-    if (Controller1->GetBumperPressed(frc::GenericHID::JoystickHand::kLeftHand)) {
-        toggleCameraMode();
-    }
+    // if (Controller1->GetBumperPressed(frc::GenericHID::JoystickHand::kLeftHand)) {
+    //     toggleCameraMode();
+    // }
 
-    float Kp = -0.1;
-    float min_command = 0.5;
+    // float Kp = -0.1;
+    // float min_command = 0.5;
 
 
-    double tx = nt::NetworkTableInstance::GetDefault().GetTable("limelight")->GetNumber("tx", 0.0);
+    // double tx = nt::NetworkTableInstance::GetDefault().GetTable("limelight")->GetNumber("tx", 0.0);
 
-    if (Controller1->GetBumper(frc::GenericHID::GenericHID::JoystickHand::kRightHand)) {
+    // if (Controller1->GetBumper(frc::GenericHID::GenericHID::JoystickHand::kRightHand)) {
 
-        double   heading_error = -tx;
-        double   steering_adjust = 0.0;
+    //     double   heading_error = -tx;
+    //     double   steering_adjust = 0.0;
 
-            if (tx > 1.0) {
-                steering_adjust = Kp*heading_error - min_command;
-            } else if (tx < 1.0) {
-                steering_adjust = Kp*heading_error + min_command;
-            }
+    //         if (tx > 1.0) {
+    //             steering_adjust = Kp*heading_error - min_command;
+    //         } else if (tx < 1.0) {
+    //             steering_adjust = Kp*heading_error + min_command;
+    //         }
 
-            drive.ArcadeDrive( 0, steering_adjust, 1 );
-    }
+    //         drive.ArcadeDrive( 0, steering_adjust, 1 );
+    // }
 }
 
 void Robot::TestInit() {}
